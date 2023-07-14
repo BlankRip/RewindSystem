264b9828785541cd6d46c99c47a04573dd937a96
BlankRip <aniketh_g5@outlook.com>
2023-07-14 10:05:18 +0530

    Testing set up

--- Assets/Scripts/RewindableItems/RewindableBall.cs
+++ Assets/Scripts/RewindableItems/RewindableBall.cs
@@ -6,6 +6,15 @@ namespace Blank.GamePlay
 {
     public class RewindableBall : RewindableBase
     {
-        
+        private Rigidbody rb;
+        [SerializeField] float initalForce = 25;
+
+        private void Start()
+        {
+            Vector3 dir = Random.onUnitSphere;
+            rb = GetComponent<Rigidbody>();
+
+            rb.AddForce((dir.normalized * initalForce), ForceMode.Impulse);
+        }
     }
 }
