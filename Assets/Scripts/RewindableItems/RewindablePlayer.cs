6bdeb846215aaa7d59f1713a3ed6006f824c6fa3
BlankRip <aniketh_g5@outlook.com>
2023-07-16 18:33:19 +0530

    Player is also rewindable

--- Assets/Scripts/RewindableItems/RewindablePlayer.cs
+++ Assets/Scripts/RewindableItems/RewindablePlayer.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Blank.RewindSystem;
 
 namespace Blank.GamePlay
 {
-    public class RewindablePlayer : MonoBehaviour
+    public class RewindablePlayer : RewindableBase
     {
+        private class RecordInstance
+        {
+            public Vector3 position;
+            public Quaternion meshRotation;
+            public float gravityVelocity;
+
+            public RecordInstance(Vector3 position, Quaternion meshRotation, float gravityVelocity)
+            {
+                this.position = position;
+                this.meshRotation = meshRotation;
+                this.gravityVelocity = gravityVelocity;
+            }
+        }
+
         [SerializeField] float speed = 10;
         [SerializeField] float gravity = -18f;
         [SerializeField] float jumpHight = 4.0f;
@@ -18,6 +33,9 @@ namespace Blank.GamePlay
         private InputAction moveAction;
         private InputAction jumpAction;
 
+        private List<RecordInstance> recordList;
+        private RecordInstance currentReadingRecordInstance;
+
         private void Start()
         {
             cc = GetComponent<CharacterController>();
@@ -27,10 +45,22 @@ namespace Blank.GamePlay
             PlayerInput playerInput = Input.GetPlayerInput();
             moveAction = playerInput.actions["Move"];
             jumpAction = playerInput.actions["Jump"];
+
+            recordList = new List<RecordInstance>();
+            recordList.Capacity = RewindHandler.maxRecordArrayLength;
+            RewindHandler.rewindCenterPoint = this.transform;
+            SubRewindEvents();
+        }
+
+        private void OnDestroy()
+        {
+            UnSubRewindEvents();
         }
 
         private void Update()
         {
+            if(RewindHandler.IsRewinding())
+                return;
             grounded = cc.isGrounded;
             Vector2 moveData = moveAction.ReadValue<Vector2>();
             HandleMovement(moveData.x, moveData.y);
@@ -54,7 +84,41 @@ namespace Blank.GamePlay
             gravityVelocity.y += gravity * Time.deltaTime;
             cc.Move(gravityVelocity * Time.deltaTime);
 
-            meshTransform.forward = moveDir.normalized;
+            if(moveDir.x != 0 || moveDir.z != 0)
+                meshTransform.forward = moveDir.normalized;
+        }
+
+        protected override void RecordData()
+        {
+            if(recordList.Count < RewindHandler.maxRecordArrayLength)
+            {
+                recordList.Insert(0, new RecordInstance(transform.position, meshTransform.rotation, gravityVelocity.y));
+            }
+            else
+            {
+                recordList.RemoveAt(recordList.Count - 1);
+                recordList.Insert(0, new RecordInstance(transform.position, meshTransform.rotation, gravityVelocity.y));
+            }
+        }
+
+        protected override void StartRewind()
+        {
+            cc.enabled = false;
+        }
+
+        protected override void Rewind()
+        {
+            currentReadingRecordInstance = recordList[0];
+            transform.position = currentReadingRecordInstance.position;
+            meshTransform.rotation = currentReadingRecordInstance.meshRotation;
+            if(recordList.Count > 1)
+                recordList.RemoveAt(0);
+        }
+
+        protected override void EndRewind()
+        {
+            cc.enabled = true;
+            gravityVelocity.y = currentReadingRecordInstance.gravityVelocity;
         }
     }
 }
